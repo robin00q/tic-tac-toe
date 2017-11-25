999e69f198918e0116808bb0e651d41d18362a2a
robin00q <32387014+robin00q@users.noreply.github.com>
2017-11-25 18:53:31 +0900

    Add files via upload

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import tkinter.messagebox
+
 
 def checked(i) :
       global player
@@ -9,22 +11,41 @@ def checked(i) :
       button["text"] = player 
       button["bg"] = "yellow"
 
+      
       if player == "X" :
             player = "O"
             button["bg"] = "yellow"
+           
       else :
             player = "X"
             button["bg"] = "lightgreen"
-
+      check()
+      
+      
 window = Tk()
 player = "X"
-list= []
+list = []
+wincase = [0, 1, 2, 3, 4, 5, 6, 7, 8, 0, 3, 6, 1, 4, 7, 2, 5, 8, 0, 4, 8, 2, 4, 6]
 
 for i in range(9) :
       b = Button(window, text="     ", command=lambda k=i: checked(k))
       b.grid(row=i//3, column=i%3)
       list.append(b)
+      
 
+
+def check() :
+      j=0
+      while j < 22 :
+            if list[wincase[j]]["text"] == list[wincase[j+1]]["text"] == list[wincase[j+2]]["text"] == "X" :
+                  tkinter.messagebox.showinfo("60132301이석준", "X is winner")
+                  quit()
+            elif list[wincase[j]]["text"] == list[wincase[j+1]]["text"] == list[wincase[j+2]]["text"] == "O":
+                  tkinter.messagebox.showinfo("60132301이석준", "O is winner")
+                  quit()
+            j = j+3;
+            
+                  
 window.mainloop()
 
 
